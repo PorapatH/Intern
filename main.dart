3e6f3f73854cbd270bc4353907b90d348fa2d233
PorapatH <54931436+PorapatH@users.noreply.github.com>
2021-07-14 12:25:17 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_db/providers/favpro.dart';
+import 'package:flutter_db/screen/favscreen.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_tts/flutter_tts.dart';
-import 'package:favorite_button/favorite_button.dart';
+import 'package:provider/provider.dart';
+
+import 'models/Favorite.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,62 +14,84 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) {
+          return Favoriteprovider();
+        }),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'รายการโปรด'),
       ),
-      debugShowCheckedModeBanner: false,
-      home: Home(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  final FlutterTts tts = FlutterTts();
-  final TextEditingController controller =
-      TextEditingController(text: 'Hello world');
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  Home() {
-    tts.setLanguage('en');
-    tts.setSpeechRate(0.4);
+  final String title;
+  
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void initState() {
+    super.initState();
+    Provider.of<Favoriteprovider>(context, listen: false).initData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'ข้อความ',
-            ),
-          ),
-        ),
-        Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [ 
-            ElevatedButton(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.text_fields),
               onPressed: () {
-                tts.speak(controller.text);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return FavScreen();
+                }));
               },
-              child: Text('Speak')
-          ),
-           FavoriteButton(
-                isFavorite: false,
-                // iconDisabledColor: Colors.white,
-                valueChanged: (_isFavorite) {
-                  print('Is Favorite : $_isFavorite');
-                },
+            )
+          ],
+        ),
+        body: Consumer(builder: (context, Favoriteprovider provider, child) {
+          var count = provider.fav.length; //นับจำนวนข้อมูล
+          if (count < 0) {
+            return Center(
+              child: Text(
+                "ไม่มีรายการโปรด",
+                style: TextStyle(fontSize: 30),
               ),
-          ],)
-        ],
-      ),
-    );
+            );
+          } else {
+            return ListView.builder(
+                itemCount: count,
+                itemBuilder: (context, int index) {
+                  Favorite data = provider.fav[index];
+                  return Card(
+                              elevation: 2,
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 10),
+                              child: ListTile(
+                                title: Text(data.message),
+                                onTap: () {
+                                  final FlutterTts tts = FlutterTts();
+                                  tts.speak(data.message);
+                                  
+                                },
+                              ),
+                            );  
+                });
+          }
+        }));
   }
 }
